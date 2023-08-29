97832a0eb1ba255274277a7dd84ed0645d08d17f
wakar-malik <wakarmalik9@gmail.com>
2023-08-29 16:05:53 +0530

    fix prime code

--- prime.js
+++ prime.js
@@ -1,10 +1,12 @@
-var num=7;
-var count=0;
-for(let i=1; i<=num; i++)
-{
-    if(num%i==0)
-    {
-        count++
-    }
+var num = 7;
+var count = 0;
+for (let i = 1; i <= num; i++) {
+  if (num % i == 0) {
+    count++;
+  }
 }
-(count>2)?console.log("Yes"):console.log("No");
+if (count === 2) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
